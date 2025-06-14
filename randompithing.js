5a279ea3fab684372cc0a5b2a847517720ac63b4
TayJay2013 <tayjay.craftboy13@gmail.com>
2025-06-14 23:17:13 +0100

    Create randompithing.js

---
+++ randompithing.js
@@ -0,0 +1,74 @@
+/*
+   Created with ExtForge
+   https://jwklong.github.io/extforge
+*/
+(async function(Scratch) {
+    const variables = {};
+
+
+    if (!Scratch.extensions.unsandboxed) {
+        alert("This extension needs to be unsandboxed to run!")
+        return
+    }
+
+    const ExtForge = {
+        Broadcasts: new function() {
+            this.raw_ = {};
+            this.register = (name, blocks) => {
+                this.raw_[name] = blocks;
+            };
+            this.execute = async (name) => {
+                if (this.raw_[name]) {
+                    await this.raw_[name]();
+                };
+            };
+        },
+
+        Variables: new function() {
+            this.raw_ = {};
+            this.set = (name, value) => {
+                this.raw_[name] = value;
+            };
+            this.get = (name) => {
+                return this.raw_[name] ?? null;
+            }
+        },
+
+        Utils: {
+            setList: (list, index, value) => {
+                [...list][index] = value;
+                return list;
+            },
+            lists_foreach: {
+                index: [0],
+                value: [null],
+                depth: 0
+            },
+            countString: (x, y) => {
+                return y.length == 0 ? 0 : x.split(y).length - 1
+            }
+        }
+    }
+
+    class Extension {
+        getInfo() {
+            return {
+                "id": "extensionID",
+                "name": "Extension",
+                "color1": "#0fbd8c",
+                "blocks": [{
+                    "opcode": "block_9abac905bdd340c5",
+                    "text": "pi game",
+                    "blockType": "reporter",
+                    "arguments": {}
+                }]
+            }
+        }
+        async block_9abac905bdd340c5(args) {}
+    }
+
+    let extension = new Extension();
+    // code compiled from extforge
+
+    Scratch.extensions.register(extension);
+})(Scratch);
